4b1699261f16ac78282c48df1e63442747eab195
Dario Sommer <dariosommer@hotmail.com>
2018-12-29 22:41:01 +0100

    msg

--- Autobahn/Autobahn_test/main.m
+++ Autobahn/Autobahn_test/main.m
@@ -1,43 +1,96 @@
+function main()
+%get Video Source
 videoSource = 'Video 3.0 #1_STAB.mp4';
-%videoSource = 'autobahn_qf.mp4';
+
+%starting waitbar
+percentage = 0;
+bar = waitbar(0,strcat('Loading Video... | ', {' '}, num2str(uint8(percentage*100)), '%'));
+
 %read Video
 video = VideoReader(videoSource);
 
+%Set tolerance for video
 %tolerance threshold for background
 tolerance = 25;
-
 %boxes having that distance will be joined
 bboxDistTolerance = 15;
 
-%get FrameSize
-frame1 = read(video, 1);
+%get FrameSize and start of Video
+frame1 = readFrame(video);
 pictureSize = size(frame1);
+videoStart = video.CurrentTime;
+
+%waitbar
+estimateFrames = ceil(video.FrameRate*video.Duration);
+estimateDur = 0.1/(estimateFrames+10);
+
+%get exact number of Frames
+nframes = 0;
+
+while hasFrame(video)
+    readFrame(video);
+    nframes = nframes +1;
+    %waitbar
+    percentage = percentage + estimateDur;
+    waitbar(percentage,bar,strcat('Loading Video... |', {' '}, num2str(uint8(percentage*100)), '%'));
+end
+
+%reset Video to start
+video.CurrentTime = videoStart;
+
+%waitbar
+percentage = 0.1;
+waitbar(percentage,bar,strcat('Identifying Background... |', {' '}, num2str(uint8(percentage*100)), '%'));
+estimateDur = 0.05/50;
 
 %get Background
 %take 50 frames and get mode from every pixel
 countMatrix = zeros(pictureSize(1), pictureSize(2), 50);
 for i = 1:50
-    countMatrix(1:end, 1:end, i) = rgb2gray(read(video, i*20));   
+    %Jump over 20 Frames
+    for j=1:20
+        readFrame(video);
+    end
+    countMatrix(1:end, 1:end, i) = rgb2gray(readFrame(video));
+    %waitbar
+    percentage = percentage + estimateDur;
+    waitbar(percentage,bar,strcat('Identifying Background... |', {' '}, num2str(uint8(percentage*100)), '%'));
 end
 background = zeros(pictureSize(1), pictureSize(2));
+%waitbar
+estimateDur = 0.05/pictureSize(1);
+%%get mode for every pixel
 for i = 1:pictureSize(1)
     for j = 1:pictureSize(2)
         background(i,j) = mode(squeeze(countMatrix(i,j,1:50)));
     end
+    %waitbar
+    percentage = percentage + estimateDur;
+    waitbar(percentage,bar,strcat('Identifying Background... |', {' '}, num2str(uint8(percentage*100)), '%'));
 end
-imshow(background, [0 255]);
+
+%waitbar
+percentage = 0.2;
+waitbar(percentage,bar,strcat('Detecting Lines... |', {' '}, num2str(uint8(percentage*100)), '%'));
+estimateDur = 0.05/pictureSize(1);
+
 %get detection lines
+%extract all white elements of background
 lines = background >= 250;
-lines = bwmorph(lines, 'skel', 6);
-lines = imopen(lines, strel('line', 5, 90));
-figure, imshow(lines, [0 1]);
+%extract all lines, skeletonization
+lines = skeletonization(lines, 6);
+%opening
+lines = opening(lines, strel('line', 5, 90));
+%get beginning of lines
 line1begin=0;
 line2begin=0;
 schwarz1 = 0;
 weiss1 = 0;
 schwarz2 = 0;
 for i = 1:pictureSize(1)-1
-    if (schwarz1 == 0) 
+    percentage = percentage + estimateDur;
+    waitbar(percentage,bar,strcat('Detecting Lines... |', {' '}, num2str(uint8(percentage*100)), '%'));
+    if (schwarz1 == 0)
         for j = 256:pictureSize(2)-256
             if (lines(pictureSize(1)-i, j) ~= 0)
                 schwarz1 = 1;
@@ -60,40 +113,35 @@ for i = 1:pictureSize(1)-1
                     line2begin = pictureSize(1)-i;
                 end
             end
-        end 
+        end
     end
 end
 
-videoReader = vision.VideoFileReader(videoSource);
-videoPlayer = vision.VideoPlayer('Name', 'Detected Cars');
+video.CurrentTime = videoStart;
 
-blobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
-    'AreaOutputPort', false, 'CentroidOutputPort', false, ...
-    'MinimumBlobArea', 150);
 videoPlayer = vision.VideoPlayer('Name', 'Detected Cars');
 videoPlayer.Position(3:4) = [650,400];  % window size: [width, height]
 se = strel('square', 3); % morphological filter for noise removal
 i=0;
 background = uint8(background);
 
-nframes = video.NumberOfFrames;
 fps = video.FrameRate;
 taggedCars = zeros([pictureSize(1) pictureSize(2) pictureSize(3) nframes], 'single');
-%class(read(video, 1)));
 
 mcnt = [];
-bboxCnt = 0;
-bboxCntPre = 0;
 
-while ~isDone(videoReader)
+percentage = 0.25;
+estimateDur = 0.75/nframes;
+waitbar(percentage,bar,strcat('Analysing Frames... |', {' '}, num2str(uint8(percentage*100)), '%'));
+while hasFrame(video)
     i=i+1;
-    frame = step(videoReader); % read the next video frame
+    frame = readFrame(video);
     result = frame;
-    frameGray = (rgb2gray(frame)*255);
-    backgroundSingle = single(background);
+    frameGray = rgb2gray(frame);
+    backgroundSingle = background;
     pixeldifference = frameGray(pictureSize(1), pictureSize(2))-backgroundSingle(pictureSize(1), pictureSize(2));
     % Detect the foreground in the current video frame
-    fgFrame = read(video, i);
+    fgFrame = frame;
     fgFrame = rgb2gray(fgFrame);
     fgFrame = uint8(fgFrame);
     if (pixeldifference ~= 0)
@@ -104,103 +152,156 @@ while ~isDone(videoReader)
     fg = uint8(tempbackground) - fgFrame;
     fg = (fg >= tolerance) | (fg <= -tolerance);
     % Use morphological opening to remove noise in the foreground
-    filteredForeground = fg;
-    filteredForeground = imerode(fg, se);
-    filteredForeground = imopen(filteredForeground, se);
-
+    filteredForeground = erosion(fg, se);
+    filteredForeground = opening(filteredForeground, se);
+    
     % Detect the connected components with the specified minimum area, and
     % compute their bounding boxes
-    bbox = step(blobAnalysis, filteredForeground);
+    bbox = ccl(filteredForeground);
     
-        lines = [0 line2begin pictureSize(2) line2begin; 0 line1begin pictureSize(2) line1begin];
+    lines = [0 line2begin pictureSize(2) line2begin; 0 line1begin pictureSize(2) line1begin];
     color = {'yellow', 'white'};
     cbbox = {};
     txtPos = [10 10];
     txtStr = size(bbox, 1);
     
-    cnt = {};
-    
-    bboxCnt = size(bbox, 1);
-    
-    if(bboxCnt < bboxCntPre)
-        
-    elseif(bboxCnt > bboxCntPre)
-        
-    end
-    
     %union overlapping boxes
     modified = true;
+    %repeat until no box was modified
     while (modified)
         modified = false;
-    if(bbox ~= 0)
-        for k = 1:size(bbox,1)
-            for j = k:size(bbox,1)
-                if (j<=size(bbox,1) && k<=size(bbox,1))
-                if j ~= k
-                    dist = distance(bbox(k,1), bbox(k,2), bbox(k,3), bbox(k,4),bbox(j,1), bbox(j,2), bbox(j,3), bbox(j,4));
-                    %overlap = bboxOverlapRatio(bbox(k,:), bbox(j,:));
-                    %if (overlap ~= 0)
-                    if (dist <= bboxDistTolerance)
-                        x = min(bbox(k,1), bbox(j,1));
-                        y = min(bbox(k,2), bbox(j,2));
-                        width = max(bbox(k,1)+bbox(k,3), bbox(j,1)+bbox(j,3));
-                        width = width - x;
-                        height = max(bbox(k,2)+bbox(k,4), bbox(j,2)+bbox(j,4));
-                        height = height - y;
-                        bbox(j,:)=[]; %delete box
-                        bbox(k,:)=[x y width height]; %update other box
-                        j=j-1;
-                        modified = true;
+        if(bbox ~= 0)
+            for k = 1:size(bbox,1)
+                for j = k:size(bbox,1)
+                    if (j<=size(bbox,1) && k<=size(bbox,1))
+                        if j ~= k
+                            %if boxes are in a certain distance to each
+                            %other join them
+                            dist = distance(bbox(k,1), bbox(k,2), bbox(k,3), bbox(k,4),bbox(j,1), bbox(j,2), bbox(j,3), bbox(j,4));
+                            if (dist <= bboxDistTolerance)
+                                x = min(bbox(k,1), bbox(j,1));
+                                y = min(bbox(k,2), bbox(j,2));
+                                width = max(bbox(k,1)+bbox(k,3), bbox(j,1)+bbox(j,3));
+                                width = width - x;
+                                height = max(bbox(k,2)+bbox(k,4), bbox(j,2)+bbox(j,4));
+                                height = height - y;
+                                bbox(j,:)=[]; %delete box
+                                bbox(k,:)=[x y width height]; %update other box
+                                j=j-1;
+                                modified = true;
+                            end
+                            
+                        end
                     end
-                    
-                end
                 end
             end
         end
     end
-    end
-    
-    
-    
+      
     if(bbox ~= 0)
-        vel = zeros(1, size(bbox, 1));        
         cnt = mcnt;
         for k = 1:size(bbox, 1)
             lines = [lines ; 0 bbox(k, 2)+bbox(k, 4) pictureSize(2) bbox(k, 2)+bbox(k, 4)];
             color{1, 2+k} = 'blue';
             
-            cbbox{1, k} = 'green';               
+            cbbox{1, k} = 'green';
             
-            if( (bbox(size(bbox, 1)-(k-1), 2) < line1begin) && ... 
-                    ( bbox(size(bbox, 1)-(k-1), 2) > line2begin)) 
-                %cnt(1, size(bbox, 1)-(k-1)) = cnt(1, size(bbox, 1)-(k-1))+1;
-            elseif ( bbox(size(bbox, 1)-(k-1), 2) > line2begin)
-                %vel(1, size(bbox, 1)-(k-1)) = (cnt(1, size(bbox, 1)-(k-1))/fps) * 12*3.6;
-            end 
             mcnt = cnt;
-            txtPos = [txtPos; bbox(size(bbox, 1)-(k-1), 1) bbox(size(bbox, 1)-(k-1), 2)];            
+            txtPos = [txtPos; bbox(size(bbox, 1)-(k-1), 1) bbox(size(bbox, 1)-(k-1), 2)];
             txtStr = [txtStr; k];
-       end
+        end
     end
     
-
+    
     % Draw bounding boxes around the detected cars and lines
     result = insertShape(result, 'Rectangle', bbox, 'Color', cbbox);
     result = insertShape(result, 'line', lines, 'Color', color);
-
+    
     % Display the number of cars found in the video frame
     result = insertText(result, txtPos, txtStr, 'BoxOpacity', 1, ...
         'FontSize', 14);
     
-
-    taggedCars(:,:,:,i) = result;
+    result = double(result);
     
-    percent = i/nframes;
-    disp(percent);
+    taggedCars(:,:,:,i) = result/255;
+    
+    percentage = percentage + estimateDur;
+    waitbar(percentage,bar,strcat('Analysing Frames... |', {' '}, num2str(uint8(percentage*100)), '%'));
+    
     
     %step(videoPlayer, result);  % display the results
 end
-
+percentage = 1;
+waitbar(1,bar,strcat('Finished... |', {' '}, num2str(uint8(percentage*100)), '%'));
+pause(.5);
+delete(bar);
 implay(taggedCars, fps);
+end
 
-release(videoReader); % close the video file
+%returns minimal distance between two rectangles
+function dist = distance(x1, y1, width1, height1, x2, y2, width2, height2)
+
+if (bboxOverlapRatio([x1 y1 width1 height1], [x2 y2 width2 height2])~=0)
+    dist = 0;
+else
+    
+    width1 = width1+x1;
+    height1 = height1+y1;
+    width2 = width2+x2;
+    height2 = height2+y2;
+    
+    left = width2 < x1;
+    right = width1 < x2;
+    bottom = height2 < y1;
+    top = height1 < y2;
+    
+    if (top && left)
+        dist = calcDistance(x1, height1, width2, y2);
+    elseif (left && bottom)
+        dist = calcDistance(x1, y1, width2, height2);
+    elseif (bottom && right)
+        dist = calcDistance(width1, y1, x2, height2);
+    elseif (right && top)
+        dist = calcDistance(width1, height1, x2, y2);
+    elseif (left)
+        dist = x1 - width2;
+    elseif (right)
+        dist = x2 - width1;
+    elseif (bottom)
+        dist = y1 - height2;
+    elseif (top)
+        dist = y2 - height1;
+    else
+        dist = 0;
+    end
+end
+end
+
+%euclidian distance
+function dist = calcDistance(x1, y1, x2, y2)
+dist = sqrt(double((x2-x1)^2+(y2-y1)^2));
+end
+
+%opening
+function result = opening(frame, structElement)
+    result = dilation(erosion(frame, structElement), structElement);
+end
+
+function result = erosion(frame, structElement)
+    result = imerode(frame, structElement);
+end
+
+function result = dilation(frame, structElement)
+    result = imdilate(frame, structElement);
+end
+
+%skeletonization
+function result = skeletonization(frame, repeat)
+    result = bwmorph(frame, 'skel', repeat);
+end
+
+%conected component labelling
+function result = ccl(frame)
+    blobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true,'AreaOutputPort', false, 'CentroidOutputPort', false, 'MinimumBlobArea', 150);
+    result = step(blobAnalysis, frame);
+end
